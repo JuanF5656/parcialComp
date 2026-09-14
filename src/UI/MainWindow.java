a3d6cbbec282595c053bf0880f9b24e22df1295f
JuanF5656 <juan.atehortua56@eia.edu.co>
2026-09-14 17:57:12 -0500

    UI de mision 1 y mision 3

--- src/UI/MainWindow.java
+++ src/UI/MainWindow.java
@@ -13,11 +13,9 @@ public class MainWindow extends JFrame {
 
         JTabbedPane tabs = new JTabbedPane();
 
-        tabs.addTab("Mission 1 — Minefield",
-                new MissionPlaceholderPanel("Rescuing Nina from the Minefield", "BFS & DFS"));
+        tabs.addTab("Mission 1 — Minefield", new Mision1Panel());
         tabs.addTab("Mission 2 — Claude Accounts", new Mision2Panel());
-        tabs.addTab("Mission 3 — Food Stash",
-                new MissionPlaceholderPanel("The Ultimate Food Stash", "Floyd-Warshall & Bellman-Ford"));
+        tabs.addTab("Mission 3 — Food Stash", new Mision3Panel());
         tabs.addTab("Mission 4 — Network", new Mision4Panel());
 
         add(tabs);
